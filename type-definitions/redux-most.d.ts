a6ceae5052a7e603a45f9047f385cde22bcf013a
Josh Burgess <joshburgess.webdev@gmail.com>
2016-10-08 17:15:30 -0400

    add TypeScript type definitions

---
+++ type-definitions/redux-most.d.ts
@@ -0,0 +1,16 @@
+import { Middleware, MiddlewareAPI } from 'redux';
+import { Stream } from 'most';
+
+export declare interface Epic<T> {
+  (action$: Stream<T>, store: MiddlewareAPI<any>): Stream<T>;
+}
+
+export interface EpicMiddleware<T> extends Middleware {
+  replaceEpic (nextEpic: Epic<T>): void;
+}
+
+export declare function createEpicMiddleware<T> (rootEpic: Epic<T>): EpicMiddleware<T>;
+
+export declare function combineEpics<T> (...epics: Epic<T>[]): Epic<T>;
+
+export declare function select<T> (actionType: string, stream: Stream<T>): Stream<T>;
