df0d12f13ba541f78adfe8f34b65ce9aad759c4a
joshburgess <joshburgess.webdev@gmail.com>
2017-05-24 22:24:14 -0400

    Add new BABEL_ENV=test settings to fix broken ES6 code in ava tests

--- src/select.js
+++ src/select.js
@@ -2,4 +2,4 @@ import { filter } from 'most'
 import { curry2 } from '@most/prelude'
 
 export const select = curry2((actionType, stream) =>
-  filter(({ type }) => type === actionType, stream))
+  filter(action => action.type && action.type === actionType, stream))
