732e29095762298fb3e10abae05a33af404a97db
joshburgess <joshburgess.webdev@gmail.com>
2017-05-24 22:24:14 -0400

    Rewrite select & selectArray for extra safety and add new BABEL_ENV=test settings to fix broken ES6 code in ava tests

--- src/selectArray.js
+++ src/selectArray.js
@@ -2,4 +2,4 @@ import { filter } from 'most'
 import { curry2, findIndex } from '@most/prelude'
 
 export const selectArray = curry2((actionTypes, stream) =>
-  filter(({ type }) => findIndex(type, actionTypes) !== -1, stream))
+  filter(action => action.type && findIndex(action.type, actionTypes) !== -1, stream))
