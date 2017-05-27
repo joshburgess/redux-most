0148d81761f14cf20a1350dfc9c1ceb2774e00e0
joshburgess <joshburgess.webdev@gmail.com>
2017-05-27 18:15:27 -0400

    Add more error handling to the new combineEpics

--- src/combineEpics.js
+++ src/combineEpics.js
@@ -6,7 +6,17 @@ export const combineEpics = epicsArray => (actions, store) => {
     throw new TypeError('You must provide an array of Epics to combineEpics')
   }
 
-  const callEpic = epic => epic(actions, store)
+  if (epicsArray.length < 1) {
+    throw new TypeError('The array passed to combineEpics must contain at least one Epic')
+  }
+
+  const callEpic = epic => {
+    if (typeof epic !== 'function') {
+      throw new TypeError('The array passed to combineEpics must contain only Epics (functions)')
+    }
+
+    return epic(actions, store)
+  }
 
   return mergeArray(map(callEpic, epicsArray))
 }
