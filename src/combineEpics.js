384c09f4e5afa408bffc5d5202d6defc403ccf82
joshburgess <joshburgess.webdev@gmail.com>
2017-05-29 03:13:18 -0400

    Fix combineEpics first error check

--- src/combineEpics.js
+++ src/combineEpics.js
@@ -2,7 +2,7 @@ import { mergeArray } from 'most'
 import { isArrayLike, map } from '@most/prelude'
 
 export const combineEpics = epicsArray => (actions, store) => {
-  if (epicsArray && !isArrayLike(epicsArray)) {
+  if (!epicsArray || !isArrayLike(epicsArray)) {
     throw new TypeError('You must provide an array of Epics to combineEpics')
   }
 
