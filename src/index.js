2d9c35745903e7ee2e0d58f0b535d8c3faa7d26b
joshburgess <joshburgess.webdev@gmail.com>
2017-05-29 03:15:42 -0400

    Update main index.js exports

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 export { combineEpics } from './combineEpics'
 export { createEpicMiddleware } from './createEpicMiddleware'
-export { EPIC_BEGIN, EPIC_END } from './constants'
+export { EPIC_END } from './constants'
 export { select } from './select'
 export { selectArray } from './selectArray'
-
