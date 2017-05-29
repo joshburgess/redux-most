415461ec37cd655fae0a9d991b4f58a529c0957d
joshburgess <joshburgess.webdev@gmail.com>
2017-05-29 05:59:17 -0400

    Update TypeScript definitions file

--- index.d.ts
+++ index.d.ts
@@ -11,8 +11,8 @@ export interface EpicMiddleware<T> extends Middleware {
 
 export declare function createEpicMiddleware<T> (rootEpic: Epic<T>): EpicMiddleware<T>;
 
-export declare function combineEpics<T> (...epics: Epic<T>[]): Epic<T>;
+export declare function combineEpics<T> (epicsArray: Epic<T>[]): Epic<T>;
 
 export declare function select<T> (actionType: string, stream: Stream<T>): Stream<T>;
 
-export declare function selectArray<T> (actionTypes: Array<string>, stream: Stream<T>): Stream<T>;
+export declare function selectArray<T> (actionTypes: string[], stream: Stream<T>): Stream<T>;
