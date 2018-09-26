a69036677fc7838514d12298f9dcb926685aa3a9
kanitsharma <kanitshrm09@gmail.com>
2018-09-27 00:38:49 +0530

    Fixed withState

--- src/withState.js
+++ src/withState.js
@@ -1,9 +1,7 @@
-import { sample, combineArray } from '@most/core'
-import { curry3 } from '@most/prelude'
+import { snapshot } from '@most/core';
+import { curry2 } from '@most/prelude';
 
-const flippedSampleState = curry3((f, stateStream, samplerStream) =>
-  sample(f, samplerStream, combineArray([stateStream, samplerStream])))
+const toArray = (action, state) => [state, action];
+const flip = fn => curry2((arg1, arg2) => fn(arg2, arg1));
 
-const toArray = (state, samplerStreamEvent) => [state, samplerStreamEvent]
-
-export const withState = flippedSampleState(toArray)
+export const withState = flip(snapshot(toArray));
