7bf30fa036c3166ad10f23a3aef81b3b2205b772
kanitsharma <kanitshrm09@gmail.com>
2018-10-03 23:29:51 +0530

    Updated stateStreamEnhancer and withState as @most/core doesn't support interop with observables

--- src/createStateStreamEnhancer.js
+++ src/createStateStreamEnhancer.js
@@ -1,21 +1,40 @@
-import { now, skipRepeats } from '@most/core'
+import {
+  skipRepeats,
+  MulticastSource,
+  never,
+  runEffects,
+} from '@most/core'
+import { newDefaultScheduler } from '@most/scheduler'
 import { STATE_STREAM_SYMBOL } from './constants'
 
-export const createStateStreamEnhancer = epicMiddleware => createStore =>
-  (reducer, preloadedState, enhancer) => {
-    const store = createStore(reducer, preloadedState, enhancer)
-    let dispatch = store.dispatch // eslint-disable-line fp/no-let
+const getState$ = store => {
+  const scheduler = newDefaultScheduler()
+  const state$ = new MulticastSource(never())
+  runEffects(state$, scheduler)
+  store.subscribe(() =>
+    state$.event(scheduler.currentTime(), store.getState())
+  )
+  return state$
+}
 
-    const middlewareApi = {
-      getState: store.getState,
-      dispatch: action => dispatch(action),
-      [STATE_STREAM_SYMBOL]: skipRepeats(now(store)),
-    }
+export const createStateStreamEnhancer = epicMiddleware => createStore => (
+  reducer,
+  preloadedState,
+  enhancer
+) => {
+  const store = createStore(reducer, preloadedState, enhancer)
+  let dispatch = store.dispatch // eslint-disable-line fp/no-let
 
-    dispatch = epicMiddleware(middlewareApi)(store.dispatch)
-
-    return {
-      ...store,
-      dispatch,
-    }
+  const middlewareApi = {
+    getState: store.getState,
+    dispatch: action => dispatch(action),
+    [STATE_STREAM_SYMBOL]: skipRepeats(getState$(store)),
   }
+
+  dispatch = epicMiddleware(middlewareApi)(store.dispatch)
+
+  return {
+    ...store,
+    dispatch,
+  }
+}
--- src/withState.js
+++ src/withState.js
@@ -1,7 +1,5 @@
 import { snapshot } from '@most/core'
-import { curry2 } from '@most/prelude'
 
-const toArray = (action, state) => [state, action]
-const flip = fn => curry2((arg1, arg2) => fn(arg2, arg1))
+const toArray = (state, action) => [state, action]
 
-export const withState = flip(snapshot(toArray))
+export const withState = snapshot(toArray)
