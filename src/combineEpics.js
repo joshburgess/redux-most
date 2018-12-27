34a3e617778da7974cb3398e2d9c3644fed875e1
Josh Burgess <joshburgesswebdev@gmail.com>
2018-12-26 22:13:25 -0500

    Fix types + improve combineEpics - last release before TS rewrite

--- src/combineEpics.js
+++ src/combineEpics.js
@@ -1,28 +1,37 @@
 import { mergeArray } from 'most'
 import { findIndex, map } from '@most/prelude'
 
-export const combineEpics = epicsArray => (actions, store) => {
+export const combineEpics = epicsArray => (
+  actionsStream,
+  middlewareApiOrStateStream,
+) => {
   if (!epicsArray || !Array.isArray(epicsArray)) {
     throw new TypeError('You must provide an array of Epics to combineEpics.')
   }
 
   if (epicsArray.length < 1) {
-    throw new TypeError('The array passed to combineEpics must contain at least one Epic.')
+    throw new TypeError(
+      'The array passed to combineEpics must contain at least one Epic.',
+    )
   }
 
   const callEpic = epic => {
     if (typeof epic !== 'function') {
-      throw new TypeError('The array passed to combineEpics must contain only Epics (functions).')
+      throw new TypeError(
+        'The array passed to combineEpics must contain only Epics (functions).',
+      )
     }
 
-    const out = epic(actions, store)
+    const out = epic(actionsStream, middlewareApiOrStateStream)
 
     if (!out || !out.source) {
       const epicIdentifier = epic.name
         ? `named ${epic.name}`
         : `at index ${findIndex(epic, epicsArray)} of the passed in array`
 
-      throw new TypeError(`All Epics in the array provided to combineEpics must return a stream. Check the return value of the Epic ${epicIdentifier}.`)
+      throw new TypeError(
+        `All Epics in the array provided to combineEpics must return a stream. Check the return value of the Epic ${epicIdentifier}.`,
+      )
     }
 
     return out
